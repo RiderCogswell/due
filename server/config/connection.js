b815c5df9c2344e68cbbafa77aa9d7345fc125eb
Ridercogswell <ridercogswell@gmail.com>
2022-07-16 00:55:07 -0700

    com

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+// connect mongoose to db
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/due', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
